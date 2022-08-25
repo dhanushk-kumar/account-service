7a5f400473fcfdcd00fe8d458c639ca3174d3895
dhanushk-dhanush <dhanushkumarkum@maveric-systems.com>
2022-08-25 17:19:02 +0530

    create account happy path added

--- src/main/java/com/maveric/accountservice/service/AccountServiceImpl.java
+++ src/main/java/com/maveric/accountservice/service/AccountServiceImpl.java
@@ -1,8 +1,27 @@
 package com.maveric.accountservice.service;
 
+import com.maveric.accountservice.dto.AccountDto;
+import com.maveric.accountservice.model.Account;
+import com.maveric.accountservice.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.maveric.accountservice.constants.Constants.getCurrentDateTime;
+import static com.maveric.accountservice.utility.ModelDtoTransformer.toDto;
+import static com.maveric.accountservice.utility.ModelDtoTransformer.toEntity;
 
 @Service
 public class AccountServiceImpl implements AccountService{
 
+    @Autowired
+    private AccountRepository repository;
+    @Override
+    public AccountDto createAccount(AccountDto accountDto) {
+
+        accountDto.setCreatedAt(getCurrentDateTime());
+        accountDto.setUpdatedAt(getCurrentDateTime());
+        Account account = toEntity(accountDto);
+        Account accountResult = repository.save(account);
+        return  toDto(accountResult);
+    }
 }
